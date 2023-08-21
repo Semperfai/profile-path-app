bf06a4432ea9f4fd320ac3ad297baa558558c2c7
Semperfai <svyatoslavmatyuha@gmail.com>
2023-08-21 15:18:36 +0300

    feat: create AuthLayout

--- layouts/AuthLayout.vue
+++ layouts/AuthLayout.vue
@@ -1,5 +1,41 @@
 <template>
-  <div></div>
+  <div id="AuthLayout" class="h-screen w-full flex justify-between">
+    <div class="lg:pt-7 pt-3 lg:px-12 px-6 lg:w-2/3 w-full lg:min-w-[800px]">
+      <NuxtLink
+        to="/admin"
+        class="inline-flex flex-col items-center justify-center text-xl font-bold">
+        <img
+          class="lg:w-28 w-[75px] select-none"
+          src="~/assets/images/logo.png" />
+        <p>Profile Path</p>
+      </NuxtLink>
+      <main class="w-full">
+        <div class="w-full md:max-w-[650px] max-w-[360px] mx-auto">
+          <slot />
+        </div>
+      </main>
+    </div>
+
+    <div class="lg:block hidden w-1/3 pointer-events-none">
+      <img
+        v-if="route.fullPath === '/'"
+        class="object-cover w-full h-screen select-none"
+        src="~/assets/images/side1.png" />
+      <img
+        v-else
+        class="object-cover w-full h-screen select-none"
+        src="~/assets/images/side2.png" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup>
+import { useUserStore } from '../stores/user'
+import { storeToRefs } from 'pinia'
+const userStore = useUserStore()
+const { updatedLinkId } = storeToRefs(userStore)
+
+const route = useRoute()
+
+onUnmounted(() => (updatedLinkId.value = 0))
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,36 @@
 <template>
-  <h1>INDEX PAGE</h1>
+  <AuthLayout>
+    <div class="mt-10">
+      <h1 class="lg:text-5xl sm:text-3xl text-2xl text-center font-extrabold">
+        Log in to your Profile Path
+      </h1>
+
+      <form class="mt-12">
+        <div>
+          <TextInput placeholder="Email: link@gmail.com" inputType="email" />
+        </div>
+
+        <div class="mt-4">
+          <TextInput placeholder="Password" inputType="password" />
+        </div>
+
+        <div class="mt-10">
+          <button type="submit" class="rounded-full w-full p-3 font-bold">
+            Log in
+          </button>
+        </div>
+      </form>
+
+      <div class="text-[14px] text-center pt-12">
+        Don't have an account?
+        <NuxtLink to="/register" class="text-[#8228D9] underline">
+          Sign up
+        </NuxtLink>
+      </div>
+    </div>
+  </AuthLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AuthLayout from '~/layouts/AuthLayout.vue'
+</script>
