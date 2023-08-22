d081b685824785b7ccc35bc77dcf407163d2f004
Semperfai <svyatoslavmatyuha@gmail.com>
2023-08-22 16:49:22 +0300

    feat: create LinkBox component

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <VitePwaManifest />
+  <div v-if="bgIsGray" class="bg-[#F3F3F1] fixed w-full h-full z-[-1]" />
+
   <NuxtPage />
 </template>
 <script setup lang="ts">
@@ -16,6 +18,7 @@ const {
   id,
   colors
 } = storeToRefs(userStore)
+
 const route = useRoute()
 
 const show = ref<boolean>(false)
@@ -64,12 +67,22 @@ const setColors = (): Color[] => {
   ]
 }
 
+const checkPath = (path: string) => {
+  if (path == '/' || path == '/register') {
+    bgIsGray.value = false
+    return
+  }
+  bgIsGray.value = true
+}
+
 onMounted(() => {
   colors.value = setColors()
   updatedLinkId.value = 0
   addLinkOverlay.value = false
   isPreviewOverlay.value = false
   isMobile.value = false
+
+  checkPath(route.fullPath)
 
   if ('ontouchstart' in window) {
     isMobile.value = true
---
+++ components/LinkBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-full bg-white rounded-3xl">
+    <div id="MainLinkBoxSection" class="px-8 py-5">
+      <div class="flex items-center justify-between py-1">
+        <div class="flex items-center w-full">
+          <input
+            v-if="true"
+            type="text"
+            v-model="name"
+            maxlength="18"
+            class="w-full text-sm font-semibold focus:outline-none" />
+          <div v-else class="flex items-center w-full">
+            <div
+              class="font-semibold mr-2 cursor-pointer"
+              :class="isMobile ? 'text-xl' : 'text-sm'">
+              TEST
+            </div>
+            <Icon
+              class="cursor-pointer"
+              name="octicon:pencil-24"
+              :size="isMobile ? '23' : '17'"
+              color="#676B5F" />
+          </div>
+        </div>
+        <div class="flex items-center">
+          <Icon
+            class="cursor-pointer"
+            :class="isMobile ? 'min-w-[23px]' : 'min-w-[17px]'"
+            name="mdi:bell-outline"
+            :size="isMobile ? '25' : '20'"
+            color="#676B5F" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
