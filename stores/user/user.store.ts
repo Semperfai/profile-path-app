e9eae5b433c6c330395be5145abd62407c306b5f
Svyatoslav Matyuha <svyatoslavmatyuha@gmail.com>
2023-08-27 19:32:52 +0300

    chore: create reset func for user store

--- stores/user/user.store.ts
+++ stores/user/user.store.ts
@@ -21,6 +21,22 @@ export const useUserStore = defineStore("user", {
     isPreviewOverlay: false,
   }),
   getters: {},
-  actions: {},
+  actions: {
+    resetState() {
+      this.$state.id = "" as UserId;
+      this.$state.name = "";
+      this.$state.email = "";
+      this.$state.image = "";
+      this.$state.bio = "";
+      this.$state.theme_id = "";
+      this.$state.theme = null;
+      this.$state.colors = [];
+      this.$state.allLinks = [];
+      this.$state.isMobile = false;
+      this.$state.updatedLinkId = 0;
+      this.$state.addLinkOverlay = false;
+      this.$state.isPreviewOverlay = false;
+    },
+  },
   persist: true,
 });
